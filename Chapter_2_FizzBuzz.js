d76ec3ebbd376c29e8b3669f189fd9cff42fe82e
food-hunter <116442078+food-hunter@users.noreply.github.com>
2024-11-19 19:56:19 -0500

    fizzy

---
+++ Chapter_2_FizzBuzz.js
@@ -0,0 +1,19 @@
+for (let count = 1; count < 101; count++) {
+  // console.log(count)
+
+
+  if (count % 3 === 0 && count % 5 === 0) {
+    console.log("FizzBuzz")
+  }
+
+  else if (count % 3 === 0) {
+    console.log("Fizz")
+  }
+  else if (count % 5 === 0) {
+    console.log("Buzz")
+  }
+
+
+
+}
+
